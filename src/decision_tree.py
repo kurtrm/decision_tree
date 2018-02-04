54ecf92f9c48966bbed1c1f981bb2c3749d40c20
kurt <kurtrm@gmail.com>
2018-02-03 16:55:08 -0800

    Go the skeleton class going for the Decision Tree. Still working on developing a complete understanding of how it works.

---
+++ src/decision_tree.py
@@ -0,0 +1,51 @@
+"""
+Module containing the DecisionTree class.
+"""
+
+
+class DecisionTree:
+    """
+    A crude implentation of a decision tree that can either use the Gini index
+    or entropy (information gain).
+    """
+
+    def __init__(self, method='entropy'):
+        """
+        """
+        pass
+
+    def __call__():
+        """
+        """
+        pass
+
+    def train(X, y):
+        """
+        """
+        pass
+
+    def predict():
+        """
+        """
+        pass
+
+    def _gini(labels):
+        """
+        """
+        pass
+
+    def _entropy(labels):
+        """
+        """
+        pass
+
+    def _id3():
+        """
+        """
+        pass
+
+    def _cart():
+        """
+        """
+        pass
+
