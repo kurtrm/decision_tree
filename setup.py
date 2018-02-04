473efb943a5b7296d56911e0a8b9d057f606b9dc
kurt <kurtrm@gmail.com>
2018-02-03 17:07:43 -0800

    Added a setup file.

---
+++ setup.py
@@ -0,0 +1,17 @@
+"""Setup for decision tree module."""
+from setuptools import setup
+
+
+extra_packages = {
+    'testing': ['pytest', 'pytest-cov']
+}
+
+
+setup(
+    name='Decision Tree',
+    description='Provides an implementation of a crude decision tree.',
+    version=0.0,
+    author='Kurt Maurer',
+    author_email='kurtrm@gmail.com',
+    extras_require=extra_packages
+)
