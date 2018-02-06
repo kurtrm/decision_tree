9566af42e31e128a632f39b8a2c6cfcfdf63f7bb
kurt <kurtrm@gmail.com>
2018-02-05 20:25:52 -0800

    Started working on a node class that will be central to actually forming the decision tree.

--- src/decision_tree.py
+++ src/decision_tree.py
@@ -5,6 +5,28 @@ from collections import Counter
 from math import log2
 
 
+class Node:
+    """
+    Node that contains all information required in order to make predictions and pass
+    information on for further evaluation to other nodes.
+    """
+
+    def __init__(self, threshold, samples, values, classification):
+        """
+        """
+        self.threshold = threshold
+        self.samples = samples
+        self.values = values
+        self.classification = classification
+        self.gini = self._gini(values)
+        self.left = None
+        self.right = None
+
+
+
+
+
+
 class DecisionTree:
     """
     A crude implentation of a decision tree that can either use the Gini index
@@ -57,7 +79,35 @@ class DecisionTree:
         Classification and Regression Tree implementation.
         """
 
-    def _cart_cost():
+    def _gini_cost(self, labeled_data, feature_name):
         """
+        Calculate the cost function as part of the CART algorithm.
         """
-        pass
+        minim = []
+        cost_min = float('inf')
+        len_labeled_data = len(labeled_data)
+        feature_data = [row[0][feature_name] for row in labeled_data]
+        max_feature = int(max(feature_data)) * 100
+        min_feature = int(min(feature_data)) * 100
+        span = (i / 100 for i in range(min_feature, max_feature + 1))
+        for x in span:
+            left = []
+            right = []
+            for i, num in enumerate(feature_data):
+                if num <= x:
+                    left.append(i)
+                else:
+                    right.append(i)
+            left_labels = [labeled_data[idx][1] for idx in left]
+            right_labels = [labeled_data[idx][1] for idx in right]
+            cost = len(left_labels) * self._gini(left_labels) / len_labeled_data + len(right_labels) * self._gini(right_labels) / len_labeled_data
+            if cost < cost_min:
+                cost_min = cost
+                minim = []
+                minim.append(x)
+                left_node = [labeled_data[idx] for idx in left]
+                right_node = [labeled_data[idx] for idx in right]
+            elif cost == cost_min:
+                minim.append(x)
+        avg_minimums = sum(minim) / len(minim)
+        return avg_minimums, left_node, right_node
