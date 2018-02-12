965b43c54a8ea1e0c97c4f7a6754a0da6c414fbb
kurt <kurtrm@gmail.com>
2018-02-12 10:23:27 -0800

    Got the _gini tests to pass.

--- tests/test_decision_tree.py
+++ tests/test_decision_tree.py
@@ -22,10 +22,10 @@ for x in range(150):
     table.append((range(randy), 1 - 1/randy))
 table.append(([], 0))
 
-# gini_table = []
-# for i in range(100):
-#     new_randy = random.randint(1, 20)
-#     gini_table.append((range(new_randy), 1 - sum((p / new_randy)**2 for p in range(new_randy) if p)))
+gini_table = []
+for i in range(100):
+    new_randy = random.randint(1, 20)
+    gini_table.append((range(new_randy), 1 - sum((1 / new_randy)**2 for _ in range(new_randy))))
 
 
 def test_instantiation():
@@ -56,9 +56,9 @@ def test_max_gini(val, expected, decision_tree):
     assert decision_tree._max_gini(val) == expected
 
 
-# @pytest.mark.parametrize('val, expected', gini_table)
-# def test_gini(val, expected, decision_tree):
-#     """
-#     Test that gini returns the correct val.
-#     """
-#     assert decision_tree._gini(val) == expected
+@pytest.mark.parametrize('val, expected', gini_table)
+def test_gini(val, expected, decision_tree):
+    """
+    Test that gini returns the correct val.
+    """
+    assert decision_tree._gini(val) == expected
